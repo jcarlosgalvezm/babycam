f96a7c4036bb4a7c8824529f36a91f232d890134
Carlos Gálvez <jcarlosgalvezm@gmail.com>
2017-10-26 16:25:21 +0000

    cfg refactor

--- babycam/modules/notifications.py
+++ babycam/modules/notifications.py
@@ -6,9 +6,17 @@ class Notify(object):
 
     def __init__(self):
         pass
+    
+    @staticmethod
+    def readcfg():
+        with open('config.yml', 'r', enconding='utf-8') as yml:
+            cfg = yaml.safe_load(yml)
+
+        return cfg['usercfg']['babyname'], cfg['usercfg']['appid'], cfg['usercfg']['secret']
 
     @staticmethod
     def notify():
-        app = App(appid='59e0f493a4c48a96c0d2736a', secret='b641a88dc13e74bad108186d69b767de')
-        app.notify(event_name='BabyCam', trackers={'Baby': 'Lucia'})
+        babyname, appid, secret = Notify.readcfg()
+        app = App(appid=appid, secret=secret)
+        app.notify(event_name='BabyCam', trackers={'Baby': babyname})
         time.sleep(600)
